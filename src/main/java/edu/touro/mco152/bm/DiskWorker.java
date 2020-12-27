3066d58e3d1ac5e215817be852f8df1ae0c4e5b7
jacobdadoun <jacobdadoun@gmail.com>
2020-12-27 04:05:00 -0500

    (1) DiskWorker.java is no longer dependent on GUI, DBPersistenceObserver and SlackManager. (2) UserInterface now has a showMessagePopUp() method that must be implemented and defined. (3) Moved JOptionPane pop-up to GUIBenchmark.java. (4) Observer registrations have been moved to their respective command classes. (5) SlackManager will be assigned its success/failure message in BMReadActionCommandCenter.java (6) Test added for command classes

--- src/main/java/edu/touro/mco152/bm/DiskWorker.java
+++ src/main/java/edu/touro/mco152/bm/DiskWorker.java
@@ -3,16 +3,6 @@ package edu.touro.mco152.bm;
 import edu.touro.mco152.bm.command.BMCommandCenter;
 import edu.touro.mco152.bm.command.BMReadActionCommandCenter;
 import edu.touro.mco152.bm.command.BMWriteActionCommandCenter;
-import edu.touro.mco152.bm.externalsys.SlackManager;
-import edu.touro.mco152.bm.persist.DBPersistenceObserver;
-import edu.touro.mco152.bm.persist.DiskRun;
-import edu.touro.mco152.bm.ui.Gui;
-
-import javax.swing.*;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static edu.touro.mco152.bm.App.*;
 
@@ -36,9 +26,10 @@ import static edu.touro.mco152.bm.App.*;
  * DiskWorker is now dependent on SwingWorker via an instance type passed in to its constructor.
  */
 
-public class DiskWorker extends BMSubject {
+public class DiskWorker {
 
     private static UserInterface userInterface;
+    public static BMSubject bmSubject = new BMSubject();
 
     public DiskWorker(UserInterface userInterface) {
         DiskWorker.userInterface = userInterface;
@@ -52,47 +43,24 @@ public class DiskWorker extends BMSubject {
         userInterface.cancelBenchMark(bool);
     }
 
-    static Boolean doBMLogic(){
-
-        /*
-          We 'got here' because: a) End-user clicked 'Start' on the benchmark UI,
-          which triggered the start-benchmark event associated with the App::startBenchmark()
-          method.  b) startBenchmark() then instantiated a DiskWorker, and called
-          its (super class's) execute() method, causing Swing to eventually
-          call this doInBackground() method.
-         */
-
-        Gui.updateLegend();  // init chart legend info
-
-        if (App.autoReset) {
-            App.resetTestData();
-            Gui.resetTestData();
-        }
+    public static Boolean doBMLogic(){
 
         // implement bmCommandCenter that will be assigned the respective Command Classes (i.e - write and read)
         BMCommandCenter bmCommand;
-        // implement DiskRun type to be assigned the diskrun from read and write respectively. It will then be passed to
-        // our GUI and DB Observers
-        DiskRun diskRunTemp;
 
         /*
-          The GUI allows either a write, read, or both types of BMs to be started. They are done serially.
+          The GUI allows either a write, read, or both types of BMs to be started. They are done serially. Only now,
+          we call the GUI from the command classes in order to DiskWorker independent of Gui
          */
 
         // Execute, Register and Notify
         if(App.writeTest) {
-            // Instantiation of bmCommand with
+
             bmCommand = new BMWriteActionCommandCenter(userInterface, numOfMarks, numOfBlocks, blockSizeKb, blockSequence);
             if(bmCommand.doBMCommand()){
-                diskRunTemp = bmCommand.getDiskRun();
-                //Persist info about the Write BM Run (e.g. into Derby Database)
-                DiskWorker.registerObserver(new DBPersistenceObserver(diskRunTemp));
-
-                Gui gui = new Gui(diskRunTemp);
-                DiskWorker.registerObserver(gui);
+                BMSubject.notifyObservers();
             }
 
-            notifyObservers();
         }
 
         /*
@@ -103,30 +71,16 @@ public class DiskWorker extends BMSubject {
 
         // try renaming all files to clear cache
         if (App.readTest && App.writeTest && !userInterface.isBenchMarkCancelled()) {
-            JOptionPane.showMessageDialog(Gui.mainFrame,
-                    "For valid READ measurements please clear the disk cache by\n" +
-                            "using the included RAMMap.exe or flushmem.exe utilities.\n" +
-                            "Removable drives can be disconnected and reconnected.\n" +
-                            "For system drives use the WRITE and READ operations \n" +
-                            "independantly by doing a cold reboot after the WRITE",
-                    "Clear Disk Cache Now", JOptionPane.PLAIN_MESSAGE);
+            userInterface.showMessagePopUp();
         }
 
         // Execute and Register. Then instantiate for slack and send a message when read is complete and Notify.
         if (App.readTest) {
             bmCommand = new BMReadActionCommandCenter(userInterface, numOfMarks, numOfBlocks, blockSizeKb, blockSequence);
             if(bmCommand.doBMCommand()){
-                diskRunTemp = bmCommand.getDiskRun();
-                //Persist info about the Write BM Run (e.g. into Derby Database)
-                DiskWorker.registerObserver(new DBPersistenceObserver(diskRunTemp));
-
-                Gui gui = new Gui(diskRunTemp);
-                DiskWorker.registerObserver(gui);
+                BMSubject.notifyObservers();
             }
-            SlackManager slackManager = new SlackManager("BadBM");
-            slackManager.setMessage(":smile: Benchmark completed");
-            DiskWorker.registerObserver(slackManager);
-            notifyObservers();
+
         }
 
         App.nextMarkNumber += App.numOfMarks;
--- src/main/java/edu/touro/mco152/bm/UserInterface.java
+++ src/main/java/edu/touro/mco152/bm/UserInterface.java
@@ -14,4 +14,6 @@ public interface UserInterface {
 
     boolean cancelBenchMark(boolean bool);
 
+    void showMessagePopUp();
+
 }
